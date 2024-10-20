f403a70a565c5e5efe16069e8304488289ac118c
Hari-P-T <112808592+Hari-P-T@users.noreply.github.com>
2024-10-20 17:23:45 +0530

    Create Collector.h

---
+++ Collector.h
@@ -0,0 +1,27 @@
+#ifndef COLLECTOR_H
+#define COLLECTOR_H
+
+#include <stddef.h>
+
+typedef struct Trash {
+    void* pointer;
+    struct Trash* next;
+} Trash;
+
+typedef struct {
+    Trash* head;
+    size_t size;
+} Trash_Stack;
+
+extern Trash_Stack Collector;
+
+void push(void* ptr);
+void pop();
+int is_empty();
+void clean();
+void* custom_malloc(size_t size);
+void* custom_calloc(size_t num, size_t size);
+void* custom_realloc(void* ptr, size_t size);
+void custom_free(void* ptr);
+
+#endif
